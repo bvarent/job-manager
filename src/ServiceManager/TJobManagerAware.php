ecf75c260436c9d7e618bf2d3d6ab8c13c2699a2
bvarent <r.arents@bva-auctions.com>
2015-07-22 11:01:46 +0200

    add trait to implement IJobManagerAware

---
+++ src/ServiceManager/TJobManagerAware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bvarent\JobManager\ServiceManager;
+
+use Bvarent\JobManager\Service\JobManager;
+
+/**
+ * Implements IJobManagerAware
+ *
+ * @author Roel Arents <r.arents@bva-auctions.com>
+ */
+trait TJobManagerAware
+{
+
+    /**
+     * @var JobManager
+     */
+    protected $jobManager;
+    
+    public function setJobManager(JobManager $jobManager)
+    {
+        $this->jobManager = $jobManager;
+    }
+
+    public function getJobManager()
+    {
+        return $this->jobManager;
+    }
+}
